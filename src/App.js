8e2af5be29db93b87fa9690bf40fe065cb8e824a
신원준 <dnjswns0930@gmail.com>
2020-05-02 23:51:11 +0900

    GonggonNavigation for Desktop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,74 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Switch
+} from "react-router-dom";
 import "./styles.css";
+import GonggongNav from "./components/GonggongNav";
+
+const navProps = {
+  logoSrc: "/logo.png",
+  menu: [
+    {
+      name: "Link1",
+      link: "#",
+      subMenu: [
+        {
+          name: "Link1-1",
+          link: "#"
+        },
+        {
+          name: "Link1-2",
+          link: "#"
+        },
+        {
+          name: "Link1-3",
+          link: "#"
+        }
+      ]
+    },
+    {
+      name: "Link2",
+      link: "#"
+    },
+    {
+      name: "Link3",
+      link: "#"
+    }
+  ]
+};
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Router>
+        <Switch>
+          <Route path="/type">
+            <Switch>
+              <Route path="/type/gonggong">
+                <GonggongNav {...navProps} />
+              </Route>
+            </Switch>
+          </Route>
+        </Switch>
+        <main>
+          <h1>Hello Navigations</h1>
+          <article className="link-container">
+            <NavLink
+              to="/type/gonggong"
+              activeStyle={{
+                fontWeight: 600,
+                color: "red",
+                textDecoration: "none"
+              }}
+            >
+              공공
+            </NavLink>
+          </article>
+        </main>
+      </Router>
     </div>
   );
 }
